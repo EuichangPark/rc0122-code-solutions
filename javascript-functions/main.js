9ac72a250a0b92bb99f3165f928f3340cd32728d
Euichang Park <euichangpark@gmail.com>
2022-02-01 15:27:55 -0500

    Completed javascript-functions exercise.

---
+++ javascript-functions/main.js
@@ -0,0 +1,24 @@
+function convertMinutesToSeconds(minutes) {
+  console.log('value of totalMinutes: ', minutes * 60);
+}
+convertMinutesToSeconds(5);
+
+function greet(name) {
+  console.log('greetings: ', '"Hey, "' + name);
+}
+greet('Beavis');
+
+function getArea(width, height) {
+  console.log('value of getArea: ', width * height);
+}
+getArea(17, 42);
+
+function getFirstName(person) {
+  console.log('value of the firstName: ', person.firstName);
+}
+getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+
+function getLastElement(array) {
+  console.log('the value of getLastElement: ', array[array.length - 1]);
+}
+getLastElement(['propane', 'and', 'propane', 'accessories']);
