e3171afe2cd3bbb329a32d0f855da771492eb618
Euichang Park <euichangpark@gmail.com>
2022-02-03 22:09:12 -0500

    Completed object-manipulation-1 exercise.

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -2,3 +2,6 @@
 function setValue(object, key, value) {
   object[key] = value;
 }
+
+//  define a function, setValue with 3 parameters "object", "key", and "value"
+//  Use a bracket notation to add key and value to the object
--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -4,3 +4,8 @@ function toObject(keyValuePair) {
   object[keyValuePair[0]] = keyValuePair[1];
   return object;
 }
+
+//  define a function named toObject with a single parameter "keyValuePari"
+//  define a variable with a bracket literal
+//  Use bracket notation to add key and value to the object
+//  return the object
