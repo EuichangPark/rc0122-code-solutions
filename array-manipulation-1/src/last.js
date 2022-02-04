56fe6711f863f444f373c7c8d781a7e87bc72cbf
Euichang Park <euichangpark@gmail.com>
2022-02-03 20:50:29 -0500

    Completed array-manipulation-1 challenge.

---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,7 @@
+/* exported last */
+function last(array) {
+  return array[array.length - 1];
+}
+
+//  define a function, last, which take a single parameter, "array"
+//  return the value of the last index of the "array"
