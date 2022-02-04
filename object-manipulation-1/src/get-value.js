ad98604c6a29b54b9fe137659162558a931cc62b
Euichang Park <euichangpark@gmail.com>
2022-02-03 22:04:51 -0500

    Completed object-manipulation-1 challenge.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,7 @@
+/* exported getValue */
+function getValue(object, key) {
+  return object[key];
+}
+
+//  define a function, getValue with two parameters "object" and "key"
+//  return object at key
