d5ef9b9d90ea92698fb24a8f929756654836363a
Euichang Park <euichangpark@gmail.com>
2022-02-09 14:45:18 -0500

    Completed javascript-forms exercise.

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,19 +1,47 @@
-var $focus = document.querySelector('#user-name');
-console.log($focus);
-$focus.addEventListener('focus', function (event) {
+var $userName = document.querySelector('#user-name');
+console.log($userName);
+$userName.addEventListener('focus', function (event) {
   console.log('focus event fired');
   console.log(event.target.name);
 });
 
-var $blur = document.querySelector('#user-email');
-console.log($blur);
-$blur.addEventListener('blur', function (event) {
+$userName.addEventListener('blur', function (event) {
   console.log('blur event fired');
   console.log(event.target.name);
 });
 
-var $input = document.querySelector('#user-message');
-console.log($input);
-$input.addEventListener('input', function (event) {
+$userName.addEventListener('input', function (event) {
   console.log(event.target.name, event.target.value);
+});
+
+var $userEmail = document.querySelector('#user-email');
+console.log($userEmail);
+$userEmail.addEventListener('blur', function (event) {
+  console.log('blur event fired');
+  console.log(event.target.name);
+});
+
+$userEmail.addEventListener('focus', function (event) {
+  console.log('focus event fired');
+  console.log(event.target.name);
+});
+
+$userEmail.addEventListener('input', function (event) {
+  console.log(event.target.name, event.target.value);
+});
+
+var $userMessage = document.querySelector('#user-message');
+console.log($userMessage);
+$userMessage.addEventListener('input', function (event) {
+  console.log(event.target.name, event.target.value);
+});
+
+$userMessage.addEventListener('blur', function (event) {
+  console.log('blur event fired');
+  console.log(event.target.name);
+});
+
+$userMessage.addEventListener('focus', function (event) {
+  console.log('focus event fired');
+  console.log(event.target.name);
 });
