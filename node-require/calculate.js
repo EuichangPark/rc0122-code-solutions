87010d026ab06a7d5b420e69b76bf8a4180fc1d3
Euichang Park <euichangpark@gmail.com>
2022-03-16 16:27:10 -0400

    variable assignments added

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -2,13 +2,16 @@ const add = require('./add');
 const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
+const num1 = parseInt(process.argv[2]);
+const operator = process.argv[3];
+const num2 = parseInt(process.argv[4]);
 
-if (process.argv[3] === 'plus') {
-  console.log('result: ', add(parseInt(process.argv[2]), parseInt(process.argv[4])));
-} else if (process.argv[3] === 'minus') {
-  console.log('result: ', subtract(parseInt(process.argv[2]), parseInt(process.argv[4])));
-} else if (process.argv[3] === 'times') {
-  console.log('result: ', multiply(parseInt(process.argv[2]), parseInt(process.argv[4])));
-} else if (process.argv[3] === 'over') {
-  console.log('result: ', divide(parseInt(process.argv[2]), parseInt(process.argv[4])));
+if (operator === 'plus') {
+  console.log('result: ', add(num1, num2));
+} else if (operator === 'minus') {
+  console.log('result: ', subtract(num1, num2));
+} else if (operator === 'times') {
+  console.log('result: ', multiply(num1, num2));
+} else if (operator === 'over') {
+  console.log('result: ', divide(num1, num2));
 }
