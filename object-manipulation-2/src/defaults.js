f251654da299f45d193efc1091b735b337badee3
Euichang Park <euichangpark@gmail.com>
2022-03-21 16:20:47 -0400

    Complete express-intro exercise.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,10 +1,1 @@
 /* exported defaults */
-function defaults(target, source) {
-  for (var key in source) {
-    if (!key.includes(target)) {
-      target[key] = source[key];
-    } else {
-      target.key = source.key;
-    }
-  }
-}
