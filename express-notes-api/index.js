fab4780b9ddd2f070fb196e255638285ad2072e2
Euichang Park <euichangpark@gmail.com>
2022-03-23 13:58:45 -0400

    Changes made

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -13,7 +13,7 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
-  if (!Number.isInteger(id) <= 0 || id <= 0) {
+  if (!Number.isInteger(id) || id <= 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!data.notes[id]) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
@@ -75,7 +75,7 @@ app.put('/api/notes/:id', (req, res) => {
         console.error(err);
         res.status(500).json({ error: 'An unexpected error occurred' });
       } else {
-        res.status(200).json(data.notes[id]);
+        res.json(data.notes[id]);
       }
     });
   }
