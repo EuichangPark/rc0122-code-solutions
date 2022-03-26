62a2cc536002ec800f20ffc0de8195f8a9b2f32f
Euichang Park <euichangpark@gmail.com>
2022-03-26 01:00:51 -0400

    app.put completed

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -40,7 +40,7 @@ app.post('/api/grades', (req, res) => {
   console.log(req.body);
   if (!req.body.name || !req.body.course || !req.body.score) {
     res.status(400).json({
-      error: 'name, course, body are required fields.'
+      error: 'name, course, score are required fields.'
     });
   } else if (!Number.isInteger(score) || score < 0 || score > 100) {
     res.status(400).json({
@@ -49,7 +49,7 @@ app.post('/api/grades', (req, res) => {
   } else {
     const sql = `
     insert into "grades" ("name","course", "score")
-    values (Kent, HTML, 80)
+    values ($1, $2, $3)
     returning *
     `;
 
@@ -64,6 +64,51 @@ app.post('/api/grades', (req, res) => {
         console.err(err);
         res.status(500).json({
           error: 'an unexpected error occurred'
+        });
+      });
+  }
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const id = Number(req.params.gradeId);
+  const score = Number(req.body.score);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({
+      error: 'id must be a positive integer.'
+    });
+  } else if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).json({
+      error: 'score must be an positive integer between 0 and 100'
+    });
+  } else if (!req.body.name || !req.body.course || !req.body.score) {
+    res.status(400).json({
+      error: 'name, course, score are required fields.'
+    });
+  } else {
+    const sql = `
+    update "grades"
+      set  "name" = $1,
+           "course" = $2,
+           "score" = $3
+     where "gradeId" = $4
+     returning *;`;
+
+    const params = [req.body.name, req.body.course, score, id];
+    db.query(sql, params)
+      .then(result => {
+        if (result.rows.length === 0) {
+          res.status(404).json({
+            error: `grade with id ${id} does not exist`
+          });
+        } else {
+          const grade = result.rows[0];
+          res.status(200).json(grade);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'an unexpected error occurred.'
         });
       });
   }
