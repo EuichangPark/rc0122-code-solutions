4fed987222f668a34e358c57860ddd40bf835bac
Euichang Park <euichangpark@gmail.com>
2022-03-27 01:33:43 -0400

    Completed sgt-back-end challenge. -Euichang Park

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const app = express();
 const pg = require('pg');
@@ -21,6 +22,7 @@ app.get('/api/grades', (req, res) => {
          "createdAt"
     from "grades"
   `;
+  console.log('Hi');
   db.query(sql)
     .then(result => {
       const grades = result.rows;
@@ -36,8 +38,6 @@ app.get('/api/grades', (req, res) => {
 
 app.post('/api/grades', (req, res) => {
   const score = Number(req.body.score);
-  // eslint-disable-next-line no-console
-  console.log(req.body);
   if (!req.body.name || !req.body.course || !req.body.score) {
     res.status(400).json({
       error: 'name, course, score are required fields.'
@@ -60,7 +60,6 @@ app.post('/api/grades', (req, res) => {
         res.status(201).json(grade);
       })
       .catch(err => {
-        // eslint-disable-next-line no-console
         console.err(err);
         res.status(500).json({
           error: 'an unexpected error occurred'
@@ -114,7 +113,36 @@ app.put('/api/grades/:gradeId', (req, res) => {
   }
 });
 
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const id = Number(req.params.gradeId);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+  } else {
+    const sql = `
+      delete from "grades"
+        where "gradeId" = ${id}
+        returning *`;
+    db.query(sql)
+      .then(result => {
+        if (result.rows.length === 0) {
+          res.status(404).json({
+            error: `gradeId ${id} doesn't exist`
+          });
+        } else {
+          res.status(204).send('Successfully deleted');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'an unexpected error occurred'
+        });
+      });
+  }
+});
+
 app.listen(3000, () => {
-  // eslint-disable-next-line no-console
   console.log('listening on 3000');
 });
