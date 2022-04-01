6aa01cd4263063a44b50ee8732f81ec70a60fdb8
Euichang Park <euichangpark@gmail.com>
2022-04-01 13:44:49 -0400

    changes made

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class ToggleSwitch extends React.Component {
   constructor(props) {
@@ -13,41 +12,15 @@ class ToggleSwitch extends React.Component {
   }
 
   render() {
-    let offOuter;
-    let offInner;
-    let onOuter;
-    let onInner;
-    let center;
-    if (this.state.isClicked === false) {
-      offOuter = 'outer off-outer';
-      offInner = 'inner off-inner';
-      center = 'center';
-      return (
-        <div className={center}>
-          <div onClick={this.handleClick} className={offOuter}>
-            <div className={offInner}></div>
+    return (
+        <div className='center'>
+          <div onClick={this.handleClick} className={`outer ${this.state.isClicked ? 'on' : 'off'}-outer`}>
+            <div className={`inner ${this.state.isClicked ? 'on' : 'off'}-inner`}></div>
           </div>
-          <h4>OFF</h4>
+          <h4>{this.state.isClicked ? 'ON' : 'OFF'}</h4>
         </div>
-      );
-    }
-    if (this.state.isClicked === true) {
-      onOuter = 'outer on-outer';
-      onInner = 'inner on-inner';
-      center = 'center';
-      return (
-        <div className={center}>
-          <div onClick={this.handleClick} className={onOuter}>
-            <div className={onInner}></div>
-          </div>
-          <h4>ON</h4>
-        </div>
-      );
-    }
+    );
   }
 }
-
-ReactDOM.render(<ToggleSwitch />,
-  document.querySelector('#root'));
 
 export default ToggleSwitch;
