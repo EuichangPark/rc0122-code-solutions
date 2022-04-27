fe9e2f38eb6097e74fdfd3a13d8d8327d7d7608e
Euichang Park <euichangpark@gmail.com>
2022-04-27 12:46:35 -0400

    Completed linked-lists exercise.

---
+++ data-structures-linked-lists/includes.js
@@ -0,0 +1,15 @@
+/* exported includes */
+
+function includes(list, value) {
+  let current = list;
+
+  while (current) {
+    if (current.data === value) {
+      return true;
+    }
+    current = current.next;
+
+  }
+
+  return false;
+}
